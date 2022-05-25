a3838e0c42dc030eaa473f6cae524380e1e0d281
nguyenvanbinh981988 <binhnguyenvan981988@gmail.com>
2022-05-25 16:30:21 +0700

    new

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] list = {6, 5, -3, 8, 10, 25, 27, -5};
+        bubbleSort(list);
+        for (int i:list) {
+            System.out.print(" "+i);
+        }
+
+    }
+
+
+    public static int[] bubbleSort(int[] list) {
+        boolean needNodePass = true;
+        for (int i = 0; i < list.length; i++) {
+            needNodePass = false;
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[i] > list[j]) {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        return list;
+    }
+}
+
